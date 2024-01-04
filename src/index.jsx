ef6c0aface7474507eab8b0d97486f62b583deab
kgy8987 <rlarldud8987@naver.com>
2024-01-04 12:58:05 +0900

    :art: index.css삭제 및 App.jsx수정

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { StrictMode } from 'react';
+import './App.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
