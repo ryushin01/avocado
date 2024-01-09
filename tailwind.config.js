c247476f8b5628268e21eca6a18ad5e6489dc882
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2024-01-09 13:07:32 +0900

    💄 테마 컬러 및 레이아웃 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: '#FF5100',
+      secondary: '#76CC1C',
+      grayscaleA: '#000',
+      grayscaleB: '#111',
+      grayscaleC: '#666',
+      grayscaleD: '#999',
+      grayscaleE: '#777',
+      grayscaleF: '#eee',
+    },
     extend: {},
   },
   plugins: [],
