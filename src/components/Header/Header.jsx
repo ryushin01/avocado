d0b7ee39a2f6f31970b18a9044286740c750432d
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2024-01-09 16:41:12 +0900

    🚑️ config의 colors 설정에 따른 에러 처리

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as IconMenu } from '../../images/header/icon_menu.svg';
 const Header = ({ isOverHero }) => {
   return (
     <header
-      className={`fixed top-0 inset-x-0 flex justify-between items-center h-12 lg:h-20 px-5 lg:px-[60px] text-xl lg:text-3xl transition-all duration-500 ease-in-out text-white lg:bg-white lg:rounded-lg lg:overflow-hidden ${
+      className={`fixed top-0 inset-x-0 flex justify-between items-center h-12 lg:h-20 px-5 lg:px-[60px] text-xl lg:text-3xl transition-all duration-500 ease-in-out text-grayscaleG lg:bg-grayscaleG lg:rounded-lg lg:overflow-hidden ${
         isOverHero ? 'scrolled' : ''
       }`}
     >
@@ -15,7 +15,7 @@ const Header = ({ isOverHero }) => {
         type="button"
         className="flex justify-end items-center w-12 h-12 lg:w-20 lg:h-20"
       >
-        <IconMenu className="w-6 h-6 lg:w-10 lg:h-10 lg:fill-black" />
+        <IconMenu className="w-6 h-6 lg:w-10 lg:h-10 lg:fill-grayscaleA" />
       </button>
     </header>
   );
