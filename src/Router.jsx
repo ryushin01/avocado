225d73f321deb97d51108070e3e70b997c0cf3e8
kgy8987 <rlarldud8987@naver.com>
2024-01-13 21:21:42 +0900

    :sparkles: 버튼컴포넌트생성

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CommonLayout from './components/Layout/CommonLayout';
-// import Main from './pages/Main/Main';
+import Main from './pages/Main/Main';
 import InitializeScroll from './modules/InitializeScroll/InitializeScroll';
-import Test from './components/Button/Test';
 
 const Router = () => {
   return (
     <BrowserRouter basename="/avocado">
       <Routes>
         <Route element={<CommonLayout />}>
-          {/* <Route path="/" element={<Main />} /> */}
-          <Route path="/" element={<Test />} />
+          <Route path="/" element={<Main />} />
         </Route>
       </Routes>
       <InitializeScroll />
