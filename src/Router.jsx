ff47fbf0fa0d8655b32b3e2edd3bd4a1cd7110e0
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2024-01-26 06:20:26 +0900

    💡 리부트로 인한 라우트 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CommonLayout from './components/Layout/CommonLayout';
+// import Login from './pages/Login/Login';
+// import SignUp from './pages/SignUp/SignUp';
 import Main from './pages/Main/Main';
+// import List from './pages/List/List';
+// import Detail from './pages/Detail/Detail';
+
 import InitializeScroll from './modules/InitializeScroll/InitializeScroll';
 
 const Router = () => {
